5b460ac3f376d09f93fd847faed98895d0d5a36a
Seohyeon Shin <ss99x200211@gmail.com>
2024-10-06 22:18:07 +0900

    add : 다회용기 dummy data list 생성

---
+++ app/src/main/java/com/example/ourcourage/android/MultiUseList.kt
@@ -0,0 +1,17 @@
+package com.example.ourcourage.android
+
+import com.example.ourcourage.android.domain.MultiUse
+
+object MultiUseList {
+    val multiUseList = listOf(
+        MultiUse(1,"서울시청","2021-10-01","대여중",1),
+        MultiUse(2,"인천시청","2021-10-02","대여중",1),
+        MultiUse(1,"서울시청","2021-10-03","대여중",1),
+        MultiUse(2,"인천시청","2021-10-03","대여중",1),
+        MultiUse(1,"서울시청","2021-10-04","대여중",1),
+        MultiUse(1,"서울시청","2021-10-04","대여중",1),
+        MultiUse(2,"인천시청","2021-10-04","대여중",1),
+        MultiUse(1,"서울시청","2021-10-05","대여중",1),
+        MultiUse(2,"인천시청","2021-10-06","대여중",1),
+    )
+}
