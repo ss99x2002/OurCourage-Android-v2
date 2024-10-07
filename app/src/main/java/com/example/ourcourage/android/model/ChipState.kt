18dee2f920698bbd2e328c5af044f5a38bf3894f
ss99x2002 <ss99x200211@gmail.com>
2024-10-07 16:57:05 +0900

    refac : #6 ChipState text Immutable 변경

--- app/src/main/java/com/example/ourcourage/android/model/ChipState.kt
+++ app/src/main/java/com/example/ourcourage/android/model/ChipState.kt
@@ -3,6 +3,7 @@ package com.example.ourcourage.android.model
 import androidx.compose.runtime.MutableState
 
 data class ChipState(
-    var text: String,
+    val text: String,
     val isSelected: MutableState<Boolean>,
+    // unexpected changes 를 막기위해 immutable 속성 변경
 )
