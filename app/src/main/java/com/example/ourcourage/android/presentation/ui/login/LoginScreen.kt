dca4ebf91ccf3b3e3024476d87f791f9adb69bfb
ss99x2002 <ss99x200211@gmail.com>
2024-10-07 17:21:56 +0900

    add : #6 LoginScreen 파일만 추가

---
+++ app/src/main/java/com/example/ourcourage/android/presentation/ui/login/LoginScreen.kt
@@ -0,0 +1,15 @@
+package com.example.ourcourage.android.presentation.ui.login
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun LoginScreen() {
+}
+
+@Composable
+fun LoginTitleText() {
+}
+
+@Composable
+fun LoginButton() {
+}
