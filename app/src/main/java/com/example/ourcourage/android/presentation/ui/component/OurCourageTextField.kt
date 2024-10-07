b1d33d7f4eb9f739fc192d6a67c7fb501e3f3c0c
ss99x2002 <ss99x200211@gmail.com>
2024-10-07 17:00:05 +0900

    feat : #6 common component TextField isError 분기 처리 구현

--- app/src/main/java/com/example/ourcourage/android/presentation/ui/component/OurCourageTextField.kt
+++ app/src/main/java/com/example/ourcourage/android/presentation/ui/component/OurCourageTextField.kt
@@ -3,16 +3,19 @@ package com.example.ourcourage.android.presentation.ui.component
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material3.LocalTextStyle
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -31,32 +34,51 @@ fun OurCourageTextField(
     placeholder: String = "",
     hint: String = "",
     height: Int,
+    textStyle: TextStyle = LocalTextStyle.current.copy(color = Color.Black, fontSize = 24.sp),
 ) {
-    BasicTextField(
-        modifier =
-            modifier
-                .clip(RoundedCornerShape(5.dp))
-                .background(Color.White)
-                .wrapContentHeight(),
-        value = value,
-        onValueChange = onValueChange,
-        textStyle = LocalTextStyle.current.copy(fontSize = 24.sp),
-        decorationBox = { innerTextField ->
-            Box(
+    Column(modifier = modifier) {
+        if (isError) {
+            Text(
+                text = errorDescription,
+                color = Color.Red,
                 modifier =
                     Modifier
-                        .border(
-                            2.dp,
-                            Color(PrimaryBlue.value),
-                            shape = RoundedCornerShape(5.dp),
+                        .padding(start = 12.dp, top = 8.dp, bottom = 8.dp),
+            )
+        }
+        BasicTextField(
+            modifier =
+                Modifier
+                    .clip(RoundedCornerShape(10.dp))
+                    .background(Color.White)
+                    .wrapContentHeight()
+                    .fillMaxWidth(),
+            value = value,
+            onValueChange = onValueChange,
+            textStyle = textStyle,
+            decorationBox = { innerTextField ->
+                Box(
+                    modifier =
+                        Modifier
+                            .border(
+                                2.dp,
+                                if (isError) Color.Red else Color(PrimaryBlue.value),
+                                shape = RoundedCornerShape(10.dp),
+                            )
+                            .padding(24.dp),
+                ) {
+                    if (value.isEmpty()) {
+                        Text(
+                            text = placeholder,
+                            style = textStyle.copy(color = Color.Gray),
                         )
-                        .padding(24.dp),
-            ) {
-                innerTextField()
-            }
-        },
-        singleLine = singleLine,
-    )
+                    }
+                    innerTextField()
+                }
+            },
+            singleLine = singleLine,
+        )
+    }
 }
 
 @Preview
@@ -65,5 +87,6 @@ fun OurCourageTextFieldPreview() {
     OurCourageTextField(
         value = "입력하세요.",
         height = 100,
+        isError = true,
     )
 }
