fa7de8aa823135f268d474a733b0b91be469ccae
ss99x2002 <ss99x200211@gmail.com>
2024-10-11 11:29:22 +0900

    refac : #8 MultiUse data class 수정

--- app/src/main/java/com/example/ourcourage/android/MultiUseList.kt
+++ app/src/main/java/com/example/ourcourage/android/MultiUseList.kt
@@ -5,14 +5,13 @@ import com.example.ourcourage.android.domain.MultiUse
 object MultiUseList {
     val multiUseList =
         listOf(
-            MultiUse(1, "서울시청", "2021-10-01", "대여중", 1),
-            MultiUse(2, "인천시청", "2021-10-02", "대여중", 1),
-            MultiUse(1, "서울시청", "2021-10-03", "대여중", 1),
-            MultiUse(2, "인천시청", "2021-10-03", "대여중", 1),
-            MultiUse(1, "서울시청", "2021-10-04", "대여중", 1),
-            MultiUse(1, "서울시청", "2021-10-04", "대여중", 1),
-            MultiUse(2, "인천시청", "2021-10-04", "대여중", 1),
-            MultiUse(1, "서울시청", "2021-10-05", "대여중", 1),
-            MultiUse(2, "인천시청", "2021-10-06", "대여중", 1),
+            MultiUse(multiUseContainerId = 1, locationName = "블루포트 인하대점", useAt = "2024-10-10 10:45:21", status = "대여중", userId = 1, locationImageUrl = R.drawable.img_tumbler),
+            MultiUse(multiUseContainerId = 1, locationName = "블루포트 인하대점", useAt = "2024-10-10 10:45:21", status = "대여중", userId = 1, locationImageUrl = R.drawable.img_tumbler),
+            MultiUse(multiUseContainerId = 1, locationName = "블루포트 인하대점", useAt = "2024-10-10 10:45:21", status = "대여중", userId = 1, locationImageUrl = R.drawable.img_tumbler),
+            MultiUse(multiUseContainerId = 1, locationName = "블루포트 인하대점", useAt = "2024-10-10 10:45:21", status = "대여중", userId = 1, locationImageUrl = R.drawable.img_tumbler),
+            MultiUse(multiUseContainerId = 1, locationName = "블루포트 인하대점", useAt = "2024-10-10 10:45:21", status = "대여중", userId = 1, locationImageUrl = R.drawable.img_tumbler),
+            MultiUse(multiUseContainerId = 1, locationName = "블루포트 인하대점", useAt = "2024-10-10 10:45:21", status = "대여중", userId = 1, locationImageUrl = R.drawable.img_tumbler),
+            MultiUse(multiUseContainerId = 1, locationName = "블루포트 인하대점", useAt = "2024-10-10 10:45:21", status = "대여중", userId = 1, locationImageUrl = R.drawable.img_tumbler),
+            MultiUse(multiUseContainerId = 1, locationName = "블루포트 인하대점", useAt = "2024-10-10 10:45:21", status = "대여중", userId = 1, locationImageUrl = R.drawable.img_tumbler),
         )
 }
