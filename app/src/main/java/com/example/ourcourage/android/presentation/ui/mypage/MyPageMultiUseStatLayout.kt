8760401a4674fdf9211881b60914aaa576aef17a
Seohyeon Shin <ss99x200211@gmail.com>
2024-10-13 00:11:29 +0900

    add: #11 다회용기 통계 Layout 추가

---
+++ app/src/main/java/com/example/ourcourage/android/presentation/ui/mypage/MyPageMultiUseStatLayout.kt
@@ -0,0 +1,11 @@
+package com.example.ourcourage.android.presentation.ui.mypage
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun MyPageMultiUseStatLayout(
+    modifier: Modifier = Modifier,
+    title : String,
+) {
+}
