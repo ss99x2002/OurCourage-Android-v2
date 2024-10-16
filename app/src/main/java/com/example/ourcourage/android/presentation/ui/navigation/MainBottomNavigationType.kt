b5fd0adcfd3604fd6df57a45b9130e0c5382fc2b
Seohyeon Shin <ss99x200211@gmail.com>
2024-10-16 22:30:53 +0900

    refac : #15 Bottom Navigation Type / Non Bottom Navigation Type 분리

--- app/src/main/java/com/example/ourcourage/android/presentation/ui/navigation/MainBottomNavigationType.kt
+++ app/src/main/java/com/example/ourcourage/android/presentation/ui/navigation/MainBottomNavigationType.kt
@@ -2,37 +2,35 @@ package com.example.ourcourage.android.presentation.ui.navigation
 
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
-import androidx.compose.ui.graphics.vector.ImageVector
 import com.example.ourcourage.android.R
-import com.example.ourcourage.android.presentation.ui.navigation.Screen.Home.icon
 
-sealed class Screen(
+sealed class MainBottomNavigationType(
     val route: String,
     @StringRes
     val resourceId: Int,
     @DrawableRes
     val icon: Int,
 ) {
-    data object Home : Screen(
+    data object Home : MainBottomNavigationType(
         route = "home",
         resourceId = R.string.home,
         icon = R.drawable.ic_bottom_home
     )
 
-    data object Point : Screen(
+    data object Point : MainBottomNavigationType(
         route = "point",
         resourceId = R.string.point,
         icon = R.drawable.ic_bottom_point
     )
 
 
-    data object Camera : Screen(
+    data object Camera : MainBottomNavigationType(
         route = "camera",
         resourceId = R.string.camera,
         icon = R.drawable.ic_bottom_camera
     )
 
-    data object Profile : Screen(
+    data object Profile : MainBottomNavigationType(
         route = "profile",
         resourceId = R.string.profile,
         icon = R.drawable.ic_bottom_my_profile
