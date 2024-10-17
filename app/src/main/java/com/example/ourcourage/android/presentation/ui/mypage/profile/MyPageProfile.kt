677103910e70662f787fe3b783d7a9df0807142c
ss99x2002 <ss99x200211@gmail.com>
2024-10-17 09:12:24 +0900

    feat : #19 MyPage 연필 클릭 시 닉네임 수정 textField 나오도록

--- app/src/main/java/com/example/ourcourage/android/presentation/ui/mypage/profile/MyPageProfile.kt
+++ app/src/main/java/com/example/ourcourage/android/presentation/ui/mypage/profile/MyPageProfile.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
@@ -48,13 +49,13 @@ fun MyPageProfile(
         ) {
             if (isEditingNickName.value) {
                 MyPageProfileNickNameEditText(
-                    modifier = Modifier,
+                    modifier = Modifier.width(125.dp),
                 )
             } else {
                 MyPageProfileNickNameText(
                     modifier = Modifier,
                     user = user,
-                    onClickEditButton = onClickEditButton,
+                    onClickEditButton = { isEditingNickName.value = !isEditingNickName.value },
                 )
             }
 
