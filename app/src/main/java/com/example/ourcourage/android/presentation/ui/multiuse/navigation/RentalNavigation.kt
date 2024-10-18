425f0bfee02a550880054014a183f39d8b5770f0
ss99x2002 <ss99x200211@gmail.com>
2024-10-18 13:33:24 +0900

    refac : #30 RentalNavigation Graph, controller navigate 확장함수 생성

---
+++ app/src/main/java/com/example/ourcourage/android/presentation/ui/multiuse/navigation/RentalNavigation.kt
@@ -0,0 +1,23 @@
+package com.example.ourcourage.android.presentation.ui.multiuse.navigation
+
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.composable
+import com.example.ourcourage.android.presentation.ui.multiuse.rental.MultiUseRentalScreen
+import com.example.ourcourage.android.presentation.ui.navigation.type.ScreenType
+
+fun NavGraphBuilder.rentalGraph(
+    navigateHome: () -> Unit,
+) {
+    composable(
+        route = ScreenType.MultiUseRental.route,
+    ) {
+        MultiUseRentalScreen()
+    }
+}
+
+fun NavHostController.navigateRental() {
+    navigate(
+        route = ScreenType.MultiUseRental.route,
+    )
+}
