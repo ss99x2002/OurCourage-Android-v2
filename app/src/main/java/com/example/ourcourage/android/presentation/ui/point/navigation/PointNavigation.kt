71ea2ece109ccbc46b4b7aa4457a27c54b12a839
ss99x2002 <ss99x200211@gmail.com>
2024-10-18 13:32:49 +0900

    refac : #30 PointNavigation Graph, controller navigate 확장함수 생성

---
+++ app/src/main/java/com/example/ourcourage/android/presentation/ui/point/navigation/PointNavigation.kt
@@ -0,0 +1,24 @@
+package com.example.ourcourage.android.presentation.ui.point.navigation
+
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.composable
+import com.example.ourcourage.android.data.MultiUseList
+import com.example.ourcourage.android.presentation.ui.navigation.type.BottomNavType
+import com.example.ourcourage.android.presentation.ui.point.PointHistoryScreen
+
+fun NavGraphBuilder.pointGraph() {
+    composable(
+        route = BottomNavType.Point.route,
+    ) {
+        PointHistoryScreen(
+            multiUseList = MultiUseList.multiUseList,
+        )
+    }
+}
+
+fun NavHostController.navigatePoint() {
+    navigate(
+        route = BottomNavType.Point.route,
+    )
+}
