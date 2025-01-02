c951ed4b351f4d365db54a2b6d769ad1f0fa70ee
Seohyeon Shin <ss99x200211@gmail.com>
2025-01-02 23:52:33 +0900

    add : #68 base UiState 클래스 추가

---
+++ app/src/main/java/com/example/ourcourage/android/util/base/UiState.kt
@@ -0,0 +1,14 @@
+package com.example.ourcourage.android.util.base
+
+sealed class UiState<out T> {
+    object Loading : UiState<Nothing>()
+    data class Success<T>(
+        val data: T,
+    ) : UiState<T>()
+
+    data class Failure<T>(
+        val message: String?,
+        val code: Int? = null,
+        val data: T? = null
+    ) : UiState<T>()
+}
