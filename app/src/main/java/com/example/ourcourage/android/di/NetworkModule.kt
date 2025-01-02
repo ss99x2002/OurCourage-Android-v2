7ac2cd244b80891f4bcde32e20e8f775701ddcc5
Seohyeon Shin <ss99x200211@gmail.com>
2025-01-02 23:58:33 +0900

    add : #68 DI NetworkModule 추가

---
+++ app/src/main/java/com/example/ourcourage/android/di/NetworkModule.kt
@@ -0,0 +1,77 @@
+package com.example.ourcourage.android.di
+
+import com.example.ourcourage.android.data.interceptor.AuthInterceptor
+import com.google.zxing.client.android.BuildConfig
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.serialization.json.Json
+import okhttp3.Interceptor
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Converter
+import retrofit2.Retrofit
+import javax.inject.Qualifier
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(client: OkHttpClient, jsonConverter: Converter.Factory): Retrofit =
+        Retrofit.Builder()
+            .baseUrl("")
+            .client(client)
+            .addConverterFactory(jsonConverter)
+            .build()
+
+    @Provides
+    @Singleton
+    fun provideJsonConverterFactory(): Converter.Factory {
+        return Json.asConverterFactory("application/json".toMediaType())
+    }
+
+    @Provides
+    @Singleton
+    fun provideHttpClient(
+        @LoggingInterceptorQualifier loggingInterceptor: HttpLoggingInterceptor,
+        @AuthInterceptorQualifier authInterceptor: Interceptor
+    ): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor(authInterceptor)
+            .addInterceptor(loggingInterceptor)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    @AuthInterceptorQualifier
+    fun provideAuthInterceptor(interceptor: AuthInterceptor): Interceptor = interceptor
+
+    @Provides
+    @Singleton
+    @LoggingInterceptorQualifier
+    fun provideLoggingInterceptor(): HttpLoggingInterceptor {
+        return HttpLoggingInterceptor().apply {
+            level = if (BuildConfig.DEBUG) {
+                HttpLoggingInterceptor.Level.BODY
+            } else {
+                HttpLoggingInterceptor.Level.NONE
+            }
+        }
+    }
+
+    @Qualifier
+    @Retention(AnnotationRetention.BINARY)
+    annotation class AuthInterceptorQualifier
+
+    @Qualifier
+    @Retention(AnnotationRetention.BINARY)
+    annotation class LoggingInterceptorQualifier
+
+}
