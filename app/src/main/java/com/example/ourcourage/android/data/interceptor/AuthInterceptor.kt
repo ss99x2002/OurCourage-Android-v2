8b9ef5b618def87cef5d79eb7d7d9916afe25f50
Seohyeon Shin <ss99x200211@gmail.com>
2025-01-03 11:56:34 +0900

    add : AuthInterceptor, UserTokenDataSource 추가

---
+++ app/src/main/java/com/example/ourcourage/android/data/interceptor/AuthInterceptor.kt
@@ -0,0 +1,38 @@
+package com.example.ourcourage.android.data.interceptor
+
+import android.content.SharedPreferences
+import okhttp3.Interceptor
+import com.example.ourcourage.android.data.datasource.local.UserTokenDataSource
+import com.google.zxing.client.android.BuildConfig
+import okhttp3.Request
+import okhttp3.Response
+import javax.inject.Inject
+class AuthInterceptor @Inject constructor() : Interceptor {
+
+    override fun intercept(chain: Interceptor.Chain): Response {
+
+        val token = if (UserTokenDataSource.getString(USER_TOKEN).isNullOrBlank()) {
+            UserTokenDataSource.getKakaoString(USER_KAKAO_TOKEN)
+        } else {
+            "Bearer ${UserTokenDataSource.getString(USER_TOKEN)}"
+        }
+
+        val originalRequest = chain.request()
+
+        val headerRequest = originalRequest.newAuthBuilder("$token")
+        return chain.proceed(headerRequest)
+    }
+
+    private fun Request.newAuthBuilder(token: String): Request =
+        if ("".contains(url.host)) {
+            this.newBuilder()
+                .addHeader("Authorization", token)
+                .build()
+        }
+        else this
+
+    companion object {
+        const val USER_TOKEN = "userToken"
+        const val USER_KAKAO_TOKEN = "userKakaoToken"
+    }
+}
