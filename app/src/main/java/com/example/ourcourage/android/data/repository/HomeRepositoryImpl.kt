1f0bb18a3e73b87aaa9c44dd5e5d0bc3b8a1ca2d
Seohyeon Shin <ss99x200211@gmail.com>
2025-01-03 23:37:54 +0900

    add : #67 HomeRepository 구현

---
+++ app/src/main/java/com/example/ourcourage/android/data/repository/HomeRepositoryImpl.kt
@@ -0,0 +1,21 @@
+package com.example.ourcourage.android.data.repository
+
+import android.util.Log
+import com.example.ourcourage.android.data.datasource.remote.HomeRemoteDataSource
+import com.example.ourcourage.android.data.mapper.todomain.toDomain
+import com.example.ourcourage.android.domain.model.UserInfo
+import com.example.ourcourage.android.domain.repository.HomeRepository
+import javax.inject.Inject
+
+class HomeRepositoryImpl @Inject constructor(
+    private val homeRemoteDataSource: HomeRemoteDataSource
+) : HomeRepository {
+    override suspend fun fetchUserInfoInHome(): Result<UserInfo> = runCatching {
+        Log.e("hyeon",homeRemoteDataSource.fetchHomeResult().toString())
+        homeRemoteDataSource.fetchHomeResult().result.toDomain()
+    }.onSuccess {
+        Log.e("hyeon", it.toString())
+    }.onFailure {
+        Log.e("hyeon", it.message.toString())
+    }
+}
