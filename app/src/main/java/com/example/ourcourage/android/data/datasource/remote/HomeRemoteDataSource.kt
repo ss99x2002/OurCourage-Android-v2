f2b5c6ea2fbfa32c7cf20ca9e35f1fe4dedbbf15
Seohyeon Shin <ss99x200211@gmail.com>
2025-01-03 23:37:46 +0900

    add : HomeRemoteDataSource 구현

--- app/src/main/java/com/example/ourcourage/android/data/datasource/remote/HomeRemoteDataSource.kt
+++ app/src/main/java/com/example/ourcourage/android/data/datasource/remote/HomeRemoteDataSource.kt
@@ -1,4 +1,8 @@
 package com.example.ourcourage.android.data.datasource.remote
 
+import com.example.ourcourage.android.data.model.response.HomeResponseDto
+import com.example.ourcourage.android.data.model.response.HomeResultResponseDto
+
 interface HomeRemoteDataSource {
+    suspend fun fetchHomeResult() : HomeResponseDto
 }
