6f24b9738ed69cc2675a266a377e9fca83fde90a
Seohyeon Shin <ss99x200211@gmail.com>
2025-01-03 23:37:27 +0900

    add : HomeResponseDto, HomeResultResponseDto, UseResResponseDto 추가

---
+++ app/src/main/java/com/example/ourcourage/android/data/model/response/UseResResponseDto.kt
@@ -0,0 +1,20 @@
+package com.example.ourcourage.android.data.model.response
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class UseResResponseDto(
+    @SerialName("rentalLocationId")
+    val rentalLocationId: Int,
+    @SerialName("locationImageUrl")
+    val locationImageUrl: String,
+    @SerialName("locationName")
+    val locationName: String,
+    @SerialName("useAt")
+    val useAt: String,
+    @SerialName("status")
+    val status: String,
+    @SerialName("multiUseContainerId")
+    val multiUseContainerId: Int
+)
