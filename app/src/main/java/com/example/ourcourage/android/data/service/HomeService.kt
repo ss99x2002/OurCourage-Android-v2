9ac945ca4e29708e60467c7f1697bb5f83766415
Seohyeon Shin <ss99x200211@gmail.com>
2025-01-03 23:36:53 +0900

    add : #67 HomeService 추가

--- app/src/main/java/com/example/ourcourage/android/data/service/HomeService.kt
+++ app/src/main/java/com/example/ourcourage/android/data/service/HomeService.kt
@@ -1,12 +1,13 @@
 package com.example.ourcourage.android.data.service
 
+import com.example.ourcourage.android.data.model.response.HomeResponseDto
+import com.example.ourcourage.android.data.model.response.HomeResultResponseDto
 import com.example.ourcourage.android.data.util.Constants.API
 import com.example.ourcourage.android.data.util.Constants.USES
 import com.example.ourcourage.android.data.util.Constants.VERSION
 import retrofit2.http.GET
 
 interface HomeService {
-
-    @GET("$API/$VERSION/$USES")
-    suspend fun fetchUserMultiUse()
+    @GET("/$API/$VERSION/$USES")
+    suspend fun fetchHomeResult() : HomeResponseDto
 }
