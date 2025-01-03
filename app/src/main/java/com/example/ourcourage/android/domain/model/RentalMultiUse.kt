3eee0cbea10cfda573ace329827ee8da512124e2
Seohyeon Shin <ss99x200211@gmail.com>
2025-01-03 23:39:33 +0900

    add : #67 UserInfo, RentalMultiUse Domain Model 생성

---
+++ app/src/main/java/com/example/ourcourage/android/domain/model/RentalMultiUse.kt
@@ -0,0 +1,15 @@
+package com.example.ourcourage.android.domain.model
+
+import com.example.ourcourage.android.presentation.type.MultiUseType
+
+data class RentalMultiUse(
+    val rentalLocationId: Int,
+    val locationName: String,
+    val locationImageUrl: String,
+    val useAt: String,
+    val status: String,
+    val multiUseContainerId: Int
+) {
+    val multiUseContainerType : MultiUseType
+        get() = MultiUseType.fromId(multiUseContainerId)
+}
