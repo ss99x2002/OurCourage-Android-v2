18631f44d0e80ea1fcba765ffbaf2544836c7186
Seohyeon Shin <ss99x200211@gmail.com>
2025-01-04 22:19:36 +0900

    add : #72 MyPageEdit Response, Request Dto 추가

---
+++ app/src/main/java/com/example/ourcourage/android/data/model/request/MyPageEditRequestDto.kt
@@ -0,0 +1,5 @@
+package com.example.ourcourage.android.data.model.request
+
+data class MyPageEditRequestDto(
+    val editNickName: String
+)
---
+++ app/src/main/java/com/example/ourcourage/android/data/model/response/MyPageEditResultResponseDto.kt
@@ -0,0 +1,8 @@
+package com.example.ourcourage.android.data.model.response
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class MyPageEditResultResponseDto (
+    val editNickname : String
+)
