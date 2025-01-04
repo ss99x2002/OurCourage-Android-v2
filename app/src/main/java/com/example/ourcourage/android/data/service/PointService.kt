4348221387c2ea4f56f966086812fc2e5bf58aa4
Seohyeon Shin <ss99x200211@gmail.com>
2025-01-05 01:41:28 +0900

    add : #74 pointService 패러미터, 반환값 추가

--- app/src/main/java/com/example/ourcourage/android/data/service/PointService.kt
+++ app/src/main/java/com/example/ourcourage/android/data/service/PointService.kt
@@ -1,11 +1,16 @@
 package com.example.ourcourage.android.data.service
 
+import com.example.ourcourage.android.data.model.response.PointResponseDto
 import com.example.ourcourage.android.data.util.Constants.API
 import com.example.ourcourage.android.data.util.Constants.POINT
 import com.example.ourcourage.android.data.util.Constants.VERSION
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface PointService {
     @GET("$API/$VERSION/$POINT")
-    suspend fun fetchUserPointHistory()
+    suspend fun fetchPointHistory(
+        @Query("page") page: Int,
+        @Query("size") size: Int
+    ): PointResponseDto
 }
