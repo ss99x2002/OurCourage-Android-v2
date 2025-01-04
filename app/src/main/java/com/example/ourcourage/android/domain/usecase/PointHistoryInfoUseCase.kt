450f6d42dbbd5cf80c0fe57ebe4c3a095d5e33eb
Seohyeon Shin <ss99x200211@gmail.com>
2025-01-05 01:42:59 +0900

    add : #74 PointHistoryInfoUseCase 추가

---
+++ app/src/main/java/com/example/ourcourage/android/domain/usecase/PointHistoryInfoUseCase.kt
@@ -0,0 +1,13 @@
+package com.example.ourcourage.android.domain.usecase
+
+import com.example.ourcourage.android.domain.model.PointHistory
+import com.example.ourcourage.android.domain.repository.PointRepository
+import javax.inject.Inject
+
+class PointHistoryInfoUseCase @Inject constructor(
+    private val pointRepository: PointRepository
+) {
+    suspend fun invoke(page: Int, size: Int): Result<PointHistory> {
+        return pointRepository.fetchPointHistory(page, size)
+    }
+}
