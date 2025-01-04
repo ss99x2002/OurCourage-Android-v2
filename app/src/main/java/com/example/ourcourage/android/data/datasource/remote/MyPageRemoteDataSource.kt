5a78e4d7eb858639772297a17ecfebaf354769a0
Seohyeon Shin <ss99x200211@gmail.com>
2025-01-05 00:11:57 +0900

    add : #72 MyPageDataSource 추가

--- app/src/main/java/com/example/ourcourage/android/data/datasource/remote/MyPageRemoteDataSource.kt
+++ app/src/main/java/com/example/ourcourage/android/data/datasource/remote/MyPageRemoteDataSource.kt
@@ -1,7 +1,9 @@
 package com.example.ourcourage.android.data.datasource.remote
 
+import com.example.ourcourage.android.data.model.response.MyPageEditResponseDto
 import com.example.ourcourage.android.data.model.response.MyPageResponseDto
 
 interface MyPageRemoteDataSource {
     suspend fun fetchMyPageResult() : MyPageResponseDto
+    suspend fun patchUserInfoInMyPage (editNickname : String) : MyPageEditResponseDto
 }
--- app/src/main/java/com/example/ourcourage/android/data/datasource/remote/impl/MyPageRemoteDataSourceImpl.kt
+++ app/src/main/java/com/example/ourcourage/android/data/datasource/remote/impl/MyPageRemoteDataSourceImpl.kt
@@ -1,6 +1,7 @@
 package com.example.ourcourage.android.data.datasource.remote.impl
 
 import com.example.ourcourage.android.data.datasource.remote.MyPageRemoteDataSource
+import com.example.ourcourage.android.data.model.response.MyPageEditResponseDto
 import com.example.ourcourage.android.data.model.response.MyPageResponseDto
 import com.example.ourcourage.android.data.service.MyPageService
 import javax.inject.Inject
@@ -8,7 +9,12 @@ import javax.inject.Inject
 class MyPageRemoteDataSourceImpl @Inject constructor(
     private val myPageService: MyPageService
 ) : MyPageRemoteDataSource {
+
     override suspend fun fetchMyPageResult(): MyPageResponseDto {
         return myPageService.fetchUserInfoInMyPage()
+    }
+
+    override suspend fun patchUserInfoInMyPage(editNickname: String): MyPageEditResponseDto {
+        return myPageService.patchUserInfoInMyPage(editNickname)
     }
 }
