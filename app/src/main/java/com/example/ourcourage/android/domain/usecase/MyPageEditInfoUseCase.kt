b8549bf6259fca9b11cb4983054907de95a6676c
Seohyeon Shin <ss99x200211@gmail.com>
2025-01-05 00:13:19 +0900

    add : #72 MyPageEditInfoUseCase 추가

---
+++ app/src/main/java/com/example/ourcourage/android/domain/usecase/MyPageEditInfoUseCase.kt
@@ -0,0 +1,13 @@
+package com.example.ourcourage.android.domain.usecase
+
+import com.example.ourcourage.android.data.model.response.MyPageEditResultResponseDto
+import com.example.ourcourage.android.domain.repository.MyPageRepository
+import javax.inject.Inject
+
+class MyPageEditInfoUseCase @Inject constructor(
+    private val myPageRepository: MyPageRepository
+) {
+    suspend fun invoke(editNickname: String): Result<MyPageEditResultResponseDto> {
+        return myPageRepository.patchUserInfoInMyPage(editNickname)
+    }
+}
