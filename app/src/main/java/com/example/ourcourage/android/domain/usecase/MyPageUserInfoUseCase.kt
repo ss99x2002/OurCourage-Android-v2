0f27a350d69aa01a7189298a0c554916b8b7ba58
Seohyeon Shin <ss99x200211@gmail.com>
2025-01-05 00:13:08 +0900

    add : #72 MyPageUserInfoUseCase 추가

---
+++ app/src/main/java/com/example/ourcourage/android/domain/usecase/MyPageUserInfoUseCase.kt
@@ -0,0 +1,13 @@
+package com.example.ourcourage.android.domain.usecase
+
+import com.example.ourcourage.android.domain.model.MyPageUserInfo
+import com.example.ourcourage.android.domain.repository.MyPageRepository
+import javax.inject.Inject
+
+class MyPageUserInfoUseCase @Inject constructor(
+    private val myPageRepository: MyPageRepository
+) {
+    suspend fun invoke(): Result<MyPageUserInfo> {
+        return myPageRepository.fetchUserInfoInMyPage()
+    }
+}
