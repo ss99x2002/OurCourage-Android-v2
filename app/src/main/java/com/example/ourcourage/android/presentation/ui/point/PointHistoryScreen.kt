783079523d358f383a508b769e5d3000de0376f7
Seohyeon Shin <ss99x200211@gmail.com>
2025-01-05 01:43:33 +0900

    feat : #74 Point 조회 API 연결

--- app/src/main/java/com/example/ourcourage/android/presentation/ui/point/PointHistoryScreen.kt
+++ app/src/main/java/com/example/ourcourage/android/presentation/ui/point/PointHistoryScreen.kt
@@ -1,5 +1,6 @@
 package com.example.ourcourage.android.presentation.ui.point
 
+import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
@@ -13,7 +14,13 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Button
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -21,32 +28,70 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.ourcourage.android.R
 import com.example.ourcourage.android.data.datasource.local.MultiUseList
 import com.example.ourcourage.android.domain.MultiUse
+import com.example.ourcourage.android.domain.model.PointContent
 import com.example.ourcourage.android.presentation.ui.component.OurCourageTopBarText
 import com.example.ourcourage.android.ui.theme.OurCourageAndroidv2Theme
+import com.example.ourcourage.android.util.base.UiState
 
 @Composable
 fun PointHistoryScreen(
     modifier: Modifier = Modifier.fillMaxSize(),
     multiUseList: List<MultiUse>,
+    pointViewModel: PointViewModel = hiltViewModel()
 ) {
+
+    LaunchedEffect(Unit) {
+        pointViewModel.fetchPointHistory()
+    }
     PointHistoryBackgroundImage(modifier)
     Column(modifier = modifier) {
         OurCourageTopBarText(
             "포인트 내역",
         )
+        val uiState by pointViewModel.pointHistoryUiState.collectAsState()
 
-        PointHistoryList(
-            multiUseList = multiUseList,
-            modifier =
-                Modifier
-                    .padding(horizontal = 24.dp)
-                    .fillMaxWidth()
-                    .height(640.dp),
-        )
+        when (uiState) {
 
+            is UiState.Loading -> {
+                Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                    CircularProgressIndicator()
+                }
+            }
+
+            is UiState.Success -> {
+                val pointHistory = (uiState as UiState.Success).data
+                PointHistoryList(
+                    pointHistoryList = pointHistory.content,
+                    modifier = Modifier
+                        .padding(horizontal = 20.dp)
+                        .fillMaxWidth()
+                        .height(640.dp)
+                )
+
+                // 페이지네이션 처리 (Load More 버튼 추가)
+                if (!pointHistory.last) {
+                    Button(
+                        onClick = { pointViewModel.fetchPointHistory(loadMore = true) },
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(16.dp)
+                    ) {
+                        Text("더 보기")
+                    }
+                }
+            }
+
+            is UiState.Failure -> {
+                Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                    Log.e("hyeon", "Error: ${(uiState as UiState.Failure).message}")
+                    Text(text = "Error: ${(uiState as UiState.Failure).message}", color = Color.Red)
+                }
+            }
+        }
         Spacer(modifier = Modifier.height(24.dp))
     }
 }
@@ -54,18 +99,18 @@ fun PointHistoryScreen(
 @Composable
 fun PointHistoryList(
     modifier: Modifier = Modifier,
-    multiUseList: List<MultiUse>,
+    pointHistoryList: List<PointContent>,
 ) {
     LazyColumn(
         modifier = modifier,
         verticalArrangement = Arrangement.spacedBy(8.dp),
     ) {
-        items(multiUseList) { multiUse ->
+        items(pointHistoryList) { content ->
             PointHistoryItem(
-                multiUse = multiUse,
+                pointContent = content,
                 modifier =
-                    Modifier
-                        .fillMaxSize(),
+                Modifier
+                    .fillMaxSize(),
             )
         }
     }
@@ -79,10 +124,10 @@ fun PointHistoryBackgroundImage(modifier: Modifier = Modifier) {
             painter = painterResource(R.drawable.img_return_complete_top_coin_background),
             contentDescription = "ReturnTopCoinBackgroundImage",
             modifier =
-                Modifier
-                    .wrapContentWidth()
-                    .background(Color.Transparent)
-                    .height(460.dp),
+            Modifier
+                .wrapContentWidth()
+                .background(Color.Transparent)
+                .height(450.dp),
             contentScale = ContentScale.Crop,
             alignment = Alignment.TopStart,
         )
@@ -92,11 +137,11 @@ fun PointHistoryBackgroundImage(modifier: Modifier = Modifier) {
             painter = painterResource(R.drawable.img_return_complete_bottom_coin_background),
             contentDescription = "ReturnTopCoinBackgroundImage",
             modifier =
-                Modifier
-                    .wrapContentWidth()
-                    .background(Color.Transparent)
-                    .height(490.dp)
-                    .align(Alignment.BottomEnd),
+            Modifier
+                .wrapContentWidth()
+                .background(Color.Transparent)
+                .height(490.dp)
+                .align(Alignment.BottomEnd),
             contentScale = ContentScale.Crop,
         )
     }
---
+++ app/src/main/java/com/example/ourcourage/android/presentation/ui/point/PointViewModel.kt
@@ -0,0 +1,65 @@
+package com.example.ourcourage.android.presentation.ui.point
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.ourcourage.android.util.handleApiError
+import com.example.ourcourage.android.domain.model.PointHistory
+import com.example.ourcourage.android.domain.usecase.PointHistoryInfoUseCase
+import com.example.ourcourage.android.util.base.UiState
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.plus
+import javax.inject.Inject
+
+@HiltViewModel
+class PointViewModel @Inject constructor(
+    private val pointHistoryInfoUseCase: PointHistoryInfoUseCase
+) : ViewModel() {
+    private var _pointHistoryUiState = MutableStateFlow<UiState<PointHistory>>(UiState.Loading)
+    val pointHistoryUiState: StateFlow<UiState<PointHistory>> get() = _pointHistoryUiState
+
+    private var currentPage = 0
+
+    fun fetchPointHistory(loadMore: Boolean = false) {
+        checkPlusPage(loadMore)
+        viewModelScope.launch {
+            pointHistoryInfoUseCase.invoke(page = currentPage, size = PAGE_SIZE).fold(
+                onSuccess = { pointHistoryInfo ->
+                    if (loadMore) {
+                        addPointHistoryData(pointHistoryInfo)
+                    } else {
+                        _pointHistoryUiState.value = UiState.Success(pointHistoryInfo)
+                    }
+                    Log.e("hyeon", "성공 ${pointHistoryInfo}")
+                },
+                onFailure = { error ->
+                    error.handleApiError(tag = "hyeon")
+                    _pointHistoryUiState.value = UiState.Failure(error.message)
+                }
+            )
+        }
+    }
+
+    private fun addPointHistoryData(pointHistoryInfo: PointHistory) {
+        _pointHistoryUiState.value = UiState.Success(
+            (pointHistoryUiState.value as UiState.Success).data.copy(
+                content = (pointHistoryUiState.value as UiState.Success).data.content + pointHistoryInfo.content
+            )
+        )
+    }
+
+    private fun checkPlusPage(loadMore: Boolean) {
+        if (loadMore) {
+            currentPage++
+        } else {
+            currentPage = 0
+        }
+    }
+
+    companion object {
+        private const val PAGE_SIZE = 10
+    }
+}
