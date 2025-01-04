1c80f8e968174cc45e503af63e9074676dbd1be5
Seohyeon Shin <ss99x200211@gmail.com>
2025-01-05 01:42:51 +0900

    add : #74 pointRepository 추가

--- app/src/main/java/com/example/ourcourage/android/di/RepositoryModule.kt
+++ app/src/main/java/com/example/ourcourage/android/di/RepositoryModule.kt
@@ -2,8 +2,10 @@ package com.example.ourcourage.android.di
 
 import com.example.ourcourage.android.data.repository.HomeRepositoryImpl
 import com.example.ourcourage.android.data.repository.MyPageRepositoryImpl
+import com.example.ourcourage.android.data.repository.PointRepositoryImpl
 import com.example.ourcourage.android.domain.repository.HomeRepository
 import com.example.ourcourage.android.domain.repository.MyPageRepository
+import com.example.ourcourage.android.domain.repository.PointRepository
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
@@ -22,5 +24,9 @@ abstract class RepositoryModule {
     @Binds
     @Singleton
     abstract fun bindMyPageRepository(myPageRepositoryImpl: MyPageRepositoryImpl): MyPageRepository
+
+    @Binds
+    @Singleton
+    abstract fun bindPointRepository(pointRepositoryImpl: PointRepositoryImpl): PointRepository
 
 }
---
+++ app/src/main/java/com/example/ourcourage/android/domain/repository/PointRepository.kt
@@ -0,0 +1,7 @@
+package com.example.ourcourage.android.domain.repository
+
+import com.example.ourcourage.android.domain.model.PointHistory
+
+interface PointRepository {
+    suspend fun fetchPointHistory(page: Int, size: Int): Result<PointHistory>
+}
