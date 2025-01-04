db4bf6840bc0e163289432ebfef04e5598c70e70
Seohyeon Shin <ss99x200211@gmail.com>
2025-01-05 01:41:44 +0900

    add : #74 pointRemoteDataSource 추가

---
+++ app/src/main/java/com/example/ourcourage/android/data/datasource/remote/impl/PointRemoteDataSourceImpl.kt
@@ -0,0 +1,14 @@
+package com.example.ourcourage.android.data.datasource.remote.impl
+
+import com.example.ourcourage.android.data.datasource.remote.PointRemoteDataSource
+import com.example.ourcourage.android.data.model.response.PointResponseDto
+import com.example.ourcourage.android.data.service.PointService
+import javax.inject.Inject
+
+class PointRemoteDataSourceImpl @Inject constructor(
+    private val pointService: PointService
+) : PointRemoteDataSource {
+    override suspend fun fetchPointHistory(page: Int, size: Int): PointResponseDto {
+        return pointService.fetchPointHistory(page, size)
+    }
+}
--- app/src/main/java/com/example/ourcourage/android/di/DataSourceModule.kt
+++ app/src/main/java/com/example/ourcourage/android/di/DataSourceModule.kt
@@ -2,8 +2,10 @@ package com.example.ourcourage.android.di
 
 import com.example.ourcourage.android.data.datasource.remote.HomeRemoteDataSource
 import com.example.ourcourage.android.data.datasource.remote.MyPageRemoteDataSource
+import com.example.ourcourage.android.data.datasource.remote.PointRemoteDataSource
 import com.example.ourcourage.android.data.datasource.remote.impl.HomeRemoteDataSourceImpl
 import com.example.ourcourage.android.data.datasource.remote.impl.MyPageRemoteDataSourceImpl
+import com.example.ourcourage.android.data.datasource.remote.impl.PointRemoteDataSourceImpl
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
@@ -20,4 +22,7 @@ abstract class DataSourceModule {
     @Binds
     @Singleton
     abstract fun bindMyPageRemoteDataSource(myPageRemoteDataSourceImpl: MyPageRemoteDataSourceImpl) : MyPageRemoteDataSource
+    @Binds
+    @Singleton
+    abstract fun bindPointRemoteDataSource(pointRemoteDataSourceImpl: PointRemoteDataSourceImpl) : PointRemoteDataSource
 }
