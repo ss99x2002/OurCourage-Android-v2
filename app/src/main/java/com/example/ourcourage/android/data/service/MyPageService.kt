5b030218591fa6c2f12edc51ac1ff5a6b2f6bd65
Seohyeon Shin <ss99x200211@gmail.com>
2025-01-05 00:11:38 +0900

    refactor : #72 MyPageService 패러미터, 반환 값 추가

--- app/src/main/java/com/example/ourcourage/android/data/service/MyPageService.kt
+++ app/src/main/java/com/example/ourcourage/android/data/service/MyPageService.kt
@@ -1,19 +1,22 @@
 package com.example.ourcourage.android.data.service
 
+import com.example.ourcourage.android.data.model.request.MyPageEditRequestDto
+import com.example.ourcourage.android.data.model.response.MyPageEditResponseDto
 import com.example.ourcourage.android.data.model.response.MyPageResponseDto
 import com.example.ourcourage.android.data.util.Constants.API
 import com.example.ourcourage.android.data.util.Constants.EDIT
 import com.example.ourcourage.android.data.util.Constants.MY_PAGE
 import com.example.ourcourage.android.data.util.Constants.USERS
 import com.example.ourcourage.android.data.util.Constants.VERSION
+import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.PATCH
 
 interface MyPageService {
 
-    @GET("$API/$VERSION/$USERS$MY_PAGE")
+    @GET("$API/$VERSION/$USERS/$MY_PAGE")
     suspend fun fetchUserInfoInMyPage() : MyPageResponseDto
 
     @PATCH("$API/$VERSION/$USERS/$MY_PAGE/$EDIT")
-    suspend fun patchUserInfoInMyPage() // 회원 정보 수정
+    suspend fun patchUserInfoInMyPage(@Body editNickName : String) : MyPageEditResponseDto
 }
