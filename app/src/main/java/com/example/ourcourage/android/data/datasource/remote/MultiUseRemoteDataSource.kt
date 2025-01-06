79729730c23d6022988f2b483c637a125277b5c8
Seohyeon Shin <ss99x200211@gmail.com>
2025-01-06 22:37:33 +0900

    add : #71 RemoteDataSource 반납 메서드 추가

--- app/src/main/java/com/example/ourcourage/android/data/datasource/remote/MultiUseRemoteDataSource.kt
+++ app/src/main/java/com/example/ourcourage/android/data/datasource/remote/MultiUseRemoteDataSource.kt
@@ -4,8 +4,12 @@ import com.example.ourcourage.android.data.model.request.MultiUseLocationRequest
 import com.example.ourcourage.android.data.model.request.MultiUseRentalRequestDto
 import com.example.ourcourage.android.data.model.response.MultiUseLocationResponseDto
 import com.example.ourcourage.android.data.model.response.MultiUseRentalResponseDto
+import com.example.ourcourage.android.data.model.response.MultiUseReturnDetailResponseDto
+import com.example.ourcourage.android.data.model.response.MultiUseReturnResponseDto
 
 interface MultiUseRemoteDataSource {
-    suspend fun fetchLocationByQRCode(multiUseLocationRequestDto: MultiUseLocationRequestDto) : MultiUseLocationResponseDto
-    suspend fun postMultiUseRental(multiUseRentalRequestDto: MultiUseRentalRequestDto) : MultiUseRentalResponseDto
+    suspend fun fetchLocationByQRCode(multiUseLocationRequestDto: MultiUseLocationRequestDto): MultiUseLocationResponseDto
+    suspend fun postMultiUseRental(multiUseRentalRequestDto: MultiUseRentalRequestDto): MultiUseRentalResponseDto
+    suspend fun fetchMultiUseDetail(useAt: String): MultiUseReturnDetailResponseDto
+    suspend fun patchMultiUseReturn(useAt: String, returnLocationId : Int): MultiUseReturnResponseDto
 }
